28d28239c8429eff68f00efc55d8fe1c71334592
naufalfajarpratama <rluthfian@student.telkomuniversity.ac.id>
2023-02-01 10:35:26 +0700

    Tab Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
 $('.main-carousel').flickity({
-    cellAlign: 'left',
-    wrapAround: true,
-    freeScroll: true
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true
+});
+$('.mains-carousel').flickity({
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true
 });
 
-function openTabs(evt, tabsName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("tabs");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabsName).style.display = "block";
-    evt.currentTarget.className += " active";
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
