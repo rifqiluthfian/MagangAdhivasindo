b95c4b4db59054f0aff17ded00a0bd170f1a9847
naufalfajarpratama <rluthfian@student.telkomuniversity.ac.id>
2023-02-02 15:29:45 +0700

    Benerin Carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,6 @@ $(document).ready(function () {
     wrapAround: true,
     freeScroll: true
   });
-
-  
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true
-      },
-      600: {
-        items: 3,
-        nav: false
-      },
-      1000: {
-        items: 5,
-        nav: true,
-        loop: false
-      }
-    }
-  });
-
-
 });
 
 
@@ -44,8 +20,28 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 
-}
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+};
 
 
+  
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
+    },
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
+    }
+  }
+});
